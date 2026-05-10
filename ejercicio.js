87c3af2fef804d8d23455c76bb4121219d4d1799
alejodanielg20 <alejodanielg20@gmail.com>
2026-05-09 20:05:29 -0500

    agrego video de evidencia y corrijo codigo

--- ejercicio.js
+++ ejercicio.js
@@ -1,29 +1,32 @@
 function listarNumeros() {
-    for (let i = 0; i < 3; i++){
+    for (let i = 1; i <= 10; i++) {
         console.log(i);
     }
 }
 
-function ejecutar(numejercicio){
-    if (numejercicio === 1) {
-        listarNumeros();
-        }else if (numejercicio === 2) {
-            ListarNumerosReverza();
-            }else if (numejercicio === 3) {
-                listarPares();
-            }
-}
-
-
-
 function ListarNumerosReverza() {
-    for (let i= 3; i > 0; i--){
+    for (let i = 10; i >= 1; i--) {
         console.log(i);
     }
 }
 
 function listarPares() {
-    for (let i = 0; i < 10;i+=2){
+    for (let i = 2; i <= 10; i += 2) {
         console.log(i);
+    }
+}
+
+function Listarimpares() {
+    for (let i = 1; i <= 10; i += 2) {
+        console.log(i);
+    }
+}
+
+function ejecutar(numejercicio) {
+    switch (numejercicio) {
+        case 1: listarNumeros();      break;
+        case 2: ListarNumerosReverza(); break;
+        case 3: listarPares();        break;
+        case 4: Listarimpares();      break;
     }
 }
